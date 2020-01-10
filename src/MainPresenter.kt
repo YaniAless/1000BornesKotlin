5d6a52a0fe13123b329b7c16384534989f4b80bd
LukasBrass <lukas.brasseleur@gmail.com>
2020-01-10 14:09:14 +0100

    Add card class, update a lot of classes and remove errors

--- src/MainPresenter.kt
+++ src/MainPresenter.kt
@@ -6,7 +6,7 @@ const val PLAYER_NUMBER = 2
 class MainPresenter {
 
     var gameEnd : Boolean = false
-    var board = Board(playerList = Array<Player>(PLAYER_NUMBER) { i -> Player("Player $i") })
+    var board = Board(playerList = Array<Player>(PLAYER_NUMBER) { i -> Player("Player ${i+1}") })
     val mainView = MainView()
     //private var presenter: GamePresenter = GamePresenter(this)
 
@@ -24,6 +24,7 @@ class MainPresenter {
         while (gameEnd == false) {
             board.playerList.forEach {
                 mainView.askPlayerWhatDo(it)
+                gameEnd = true
             }
         }
     }
